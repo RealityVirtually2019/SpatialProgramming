1d3c774355dceddcfb54137a2bc71b967054a959
SiriusA7 <35086581+SiriusA7@users.noreply.github.com>
2019-01-19 11:15:02 -0800

    Work in progress scripts

---
+++ Assets/Scripts/BaseNode.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum dir { up, right, down, left };
+public class BaseNode : MonoBehaviour {
+
+    public string commandType;
+    public bool isConnectedLeft;
+    public bool isConnectedRight;
+  
+    public dir WorldDirections;
+    public int magnitude;
+    
+
+    void ConnectedNode()
+    {
+        if (true)
+        {
+
+        }
+    }
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/CanvasGrid.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasGrid : MonoBehaviour {
+
+    public static CanvasGrid instant;
+
+    public List<dir> directions = new List<dir>();
+    public List<int> dirValue = new List<int>();
+    public GameObject ProgAvatar;
+
+
+
+    private void Awake()
+    {
+        instant = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("directions"))
+        {
+            directions.Add(other.GetComponent<BaseNode>().WorldDirections);
+        }
+        else
+        {
+            dirValue.Add(other.GetComponent<BaseNode>().magnitude);
+        }
+    }
+
+    public void StartAvatar()
+    {
+        foreach (int element in dirValue)
+        {
+            for (int i =1;i<=dirValue.Count;i++)
+            {
+                ProgAvatar.transform.Translate(Vector3.forward);
+            }
+        }
+    }
+}
