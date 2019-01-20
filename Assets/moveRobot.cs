da6607e8b6d1af2d85383b42b17eeeb945fa97cf
realityCheckGal <realityCheckGal@users.noreply.github.com>
2019-01-19 20:35:27 -0500

    Fixed scale on apartment and robot

---
+++ Assets/moveRobot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveRobot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
